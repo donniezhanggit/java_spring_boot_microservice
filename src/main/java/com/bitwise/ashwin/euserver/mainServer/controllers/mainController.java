2877f12b853946141f8f9feb70903690a3a49a19
Ashwin Shrivastava <ashwins@bitwiseglobal.net>
2019-09-20 14:48:21 +0530

    First Commit

---
+++ src/main/java/com/bitwise/ashwin/euserver/mainServer/controllers/mainController.java
@@ -0,0 +1,15 @@
+package com.bitwise.ashwin.euserver.mainServer.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class mainController {
+	
+	@RequestMapping(value="/show")
+	@ResponseBody
+	public String show() {
+		return "<h1>This is Mars</h1>";
+	}
+}
